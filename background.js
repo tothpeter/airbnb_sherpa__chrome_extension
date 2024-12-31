444b1c81d32a7fae84be0c7b71975a9eb96e2d9f
Peter Toth <tothpeter08@gmail.com>
2025-01-01 01:33:43 +0700

    Use an observer to decorate the listings with a hide button

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("airbnb.com/s/")) {
-    chrome.tabs.sendMessage(tabId, null);
-  }
-});
--- search/content.js
+++ search/content.js
@@ -1,5 +1,28 @@
-(() => {
-  chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    console.log('------ URL CHANGED ------');
-  });
-})();
+// Listens for new search results and processes new listings
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    const addedElements = Array.from(mutation.addedNodes);
+
+    addedElements.forEach(element => {
+      if (element.getAttribute && element.getAttribute('itemprop') === 'itemListElement') {
+        processListing(element);
+      }
+    });
+  }
+ });
+
+ observer.observe(document.body, {
+  childList: true,
+  subtree: true
+ });
+
+function processListing(listing) {
+  addHideButton(listing);
+}
+
+function addHideButton(listing) {
+  const hideButton = document.createElement('button');
+  hideButton.textContent = 'Hide';
+
+  listing.appendChild(hideButton);
+}
