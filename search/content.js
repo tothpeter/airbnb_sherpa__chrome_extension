88d301002354951a6863ba88a2a2d1545b12022a
Peter Toth <tothpeter08@gmail.com>
2025-01-01 02:16:59 +0700

    Save the listing ID to local storage when the user hides a listing

--- search/content.js
+++ search/content.js
@@ -19,8 +19,23 @@ observer.observe(document.body, {
   subtree: true,
 });
 
+let hiddenListingIds;
+
+chrome.storage.local.get(['hiddenListingIds'], (result) => {
+  hiddenListingIds = result.hiddenListingIds || {};
+});
+
 function processListing(listing) {
+  hideOrShowListing(listing);
   addButtons(listing);
+}
+
+function hideOrShowListing(listing) {
+  const listingId = getListingIdFrom(listing);
+
+  if (hiddenListingIds[listingId]) {
+    listing.classList.add('hidden-listing');
+  }
 }
 
 function addButtons(listing) {
@@ -47,8 +62,33 @@ function addButtons(listing) {
 
 function hideListing(listing) {
   listing.classList.add('hidden-listing');
+
+  const listingId = getListingIdFrom(listing);
+
+  storeHiddenListingId(listingId);
 }
 
 function showListing(listing) {
   listing.classList.remove('hidden-listing');
+
+  const listingId = getListingIdFrom(listing);
+
+  removeHiddenListingId(listingId);
+}
+
+function getListingIdFrom(listing) {
+  const urlElement = listing.querySelector('[itemprop="url"]');
+  // the URL is like "www.airbnb.com/rooms/846234228646563727?adults..."
+  const url = urlElement.getAttribute('content');
+  return url.match(/\/rooms\/(\d+)/)?.[1];
+}
+
+function storeHiddenListingId(listingId) {
+  hiddenListingIds[listingId] = true;
+  chrome.storage.local.set({ hiddenListingIds });
+}
+
+function removeHiddenListingId(listingId) {
+  delete hiddenListingIds[listingId];
+  chrome.storage.local.set({ hiddenListingIds });
 }
